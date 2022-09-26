57283daf5ad676cd891f50da95798615be20bac8
MattiaMuggeri <112398934+MattiaMuggeri@users.noreply.github.com>
2022-09-26 02:16:18 -0400

    Update Debug.py

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -3,16 +3,22 @@ def main():
 
     #that's not right... how can we make sure it also includes equals 5?
     temp = 5
-    if temp > 5:
+    if temp >= 5:
         print("temp is greater than or equal to 5")
         
     
     #enter your name here
-    name = ""
+    name = "Mattia"
     '''in this if/elif/else, add 4 mispellings of your name for if/elif comparisons, and then have your last elif be elif name = "your properly spelled name" '''
-    if name == "mispelling here":
+    if name == "Matia":
         print("That's not right!")
-    elif name == "another mispelling":
+    elif name == "Matteo":
+        print("That's not right!")
+    elif name == "Matto":
+        print("That's not right!")
+    elif name == "Mattia":
+        print("That's right!")
+    else :
         print("That's not right!")
     #etc etc, the else can be whatever you want
     
@@ -22,28 +28,46 @@ def main():
     even = input(int("Enter a number to find out if its even or odd"))
     even = int(even)
     #what do we replace the question marks with?
-    if even ? ? == ?:
+    if even % 2 == 0:
         #what would be appropriate in these print statements?
-        print("")
+        print("It is even")
     else:
-        print("")
+        print("It is odd")
   
   
     #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
+    #Because index starts at 0 hence you are using the subsequent element, I changed all 2's to 1's and all 4's to 3's
     numbers = [1,2,3,4,5]
-    print(numbers[2])
-    print(numbers[4])
-    print(numbers[4] / numbers[2])
-    print(numbers[4] * numbers[2])
+    print(numbers[1])
+    print(numbers[3])
+    print(numbers[3] / numbers[1])
+    print(numbers[3] * numbers[1])
     
     #again, why is it getting 7 and 20, I wanted 3 and 29!
+    #same as before
     numbers2 = [465,3,30,7,29,20,82,13,5]
-    print(numbers2[3])
-    print(numbers2[5])
+    print(numbers2[2])
+    print(numbers2[4])
     
     #Here's a fun one: This is a list of everyone's name. Find where yours is and print the index of your name
     students = ["Achorn, Cameron", "Brown, Evan", "Catalano, Alexander", "Cianfoni, Alexander", "Delorey, Tyler","Edmonds, Jonah","Elliott, Dustin","Faix, Joseph","Fleischman, Connor","Fuerte, Caroline","Gidos, Hannah","Lavitt, Samuel","Lichstein, Harris","Longo, Nicholas","Martinez, David","Muggeri, Mattia","Munger, Ryan","Nealon, Ryan","Paulus, Natalie","Penn, Riley","Potenza, Amanda","Prochet, Carlisl","Putkaradze, Saba","Quayson, Eugene","Rietti, Cristina","Seeley, Shane","Sweeney, Quinn","Tata, Mathew","Taylor, Julia","Tuozzo, Michael"]
-    print(students[?])
+    N = len(students)
+    Found = False
+    L = N - 1
+    #The following line has a syntax error and IDK what is wrong please help
+    loop X from 0 to L:
+        if (students[X] == "Mattia):
+            print ("Found at index", X )
+            Found = True
+    end loop
+    
+    if Found == False
+            print("Not found")
+            
+        
+        
+    
+    
 main()
     
     
