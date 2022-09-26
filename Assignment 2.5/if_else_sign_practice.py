2a206d4147063dddb9058421d92d49cba592b25e
MattiaMuggeri <112398934+MattiaMuggeri@users.noreply.github.com>
2022-09-26 02:56:53 -0400

    Update if_else_sign_practice.py

--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -5,6 +5,17 @@ def main():
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
+    age = input(int("What is your age? "))
+    
+    if age >= 25:
+        print("You can buy alcohol, nicotine products, and can rent a car")
+    elif 21 <= age < 25:
+        print("You can buy alcohol, nicotine products, but cannot rent a car")
+    elif 18 <= age < 21:
+        print("You can only buy nicotine products in some states")
+    else :
+        print("You can only purchase candy cigarettes and sody pops")
+    
     
   
 
